e1b6845d91f92348fc270461ac4a54cbb86fd374
LucaZampieri <luca.zampieri@epfl.com>
2020-03-22 20:49:53 +0100

    Added changed resolution, still not working

--- nonnascan/script/digitalize.py
+++ nonnascan/script/digitalize.py
@@ -3,6 +3,10 @@ import argparse
 import subprocess
 import os
 
+from PIL import Image
+
+# import Image
+
 
 def main():
     parser = argparse.ArgumentParser(description="Digitalize a bunch of images.")
@@ -10,21 +14,32 @@ def main():
     parser.add_argument("-l", "--lang", type=str, default="eng", help="")
     args = parser.parse_args()
     data_folder = args.data
-    files = glob.glob(data_folder + "/*")
-    print(f"Analysing {files}")
-    processed_files = []
+    files = glob.glob(data_folder + "/*.jpeg")
+
+    print("Preprocessing")
+    preprocessed_files = []
     for file in files:
+        out_file = os.path.dirname(file) + "/../tmp/" + os.path.basename(file)
+        print(out_file)
+        adapt_dpi(file, out_file, 300)
+        preprocessed_files.append(out_file)
+
+    print(f"Analysing {preprocessed_files}")
+    processed_files = []
+    for file in preprocessed_files:
         file = os.path.abspath(file)
-        processed_files.append(digitalize_file(file, lang=args.lang))
-    for i, _ in enumerate(files):
+        content = digitalize_file(file, lang=args.lang)
+        processed_files.append(content)
+    for i, x in enumerate(processed_files):
         print(f"File {i} --- results ---")
+        print(x)
 
 
 def digitalize_file(file_path, lang=""):
 
     # TODO accept many languages
     language = f"-l {lang}"  # "-l eng" # pass as an argument
-    result_file = os.path.dirname(file_path) + "/tmp"
+    result_file = file_path[: file_path.rfind(".")]
 
     print(result_file)
     tesseract_cmd = f"tesseract {file_path} {result_file} {language}"
@@ -33,6 +48,11 @@ def digitalize_file(file_path, lang=""):
     with open(result_file + ".txt", "r") as f:
         content = f.read()
     return content
+
+
+def adapt_dpi(in_file, out_file, dpi=300):
+    im = Image.open(in_file)
+    im.save(out_file, dpi=(dpi, dpi))
 
 
 if __name__ == "__main__":
